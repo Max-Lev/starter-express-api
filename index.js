a8c44e571b194ea7b9235d62f9886a2d2e4b4a42
Max-Lev <maxlevtov@gmail.com>
2023-06-21 15:57:19 +0300

    test

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 // app.use(__dirname, 'index.html');
 // console.log(__dirname);
 // app.use(express.static('index.html'));
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    // res.send('Yo!')
-    res.sendFile('index.html', { root: __dirname })
-})
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     // res.send('Yo!')
+//     res.sendFile('index.html', { root: __dirname })
+// })
 app.all('/message', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!');
+    res.json({
+        name:'Max'
+    });
 })
 app.listen(process.env.PORT || 3000)
